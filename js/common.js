33c703532ffa07c87e93d8d0e6cdd4e58c2c7b6e
ko <ljs5923@naver.com>
2018-08-05 02:50:57 +0900

    ko

--- js/common.js
+++ js/common.js
@@ -108,26 +108,4 @@ jQuery(function ($) {
         }
     )
     //grid _cont_txt event end
-    //grid event start
-    var $gird = $('.grid').masonry({
-        itemSelector: '.grid-item',
-        columnWidth: 282,
-        gutter: 24
-    });
-    $gird.imagesLoaded().progress(function () {
-        $gird.masonry('layout');
-    })
-    var moreIndex = 1
-    var gridIndex = Math.ceil($('grid-item').length / 8);
-    $('._btn_more').on('click', function () {
-        for (i = 0; i < 8; i++) {
-            $('.grid-item').eq(i + (moreIndex * 8)).fadeIn();
-        }
-        $gird.masonry('layout');
-        if (gridIndex == moreIndex) {
-            return false;
-        }
-        ++moreIndex;
-    })
-    //grid event end
 }); //jQuery End
